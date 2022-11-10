afa6f57f4e3de4d6bd75a7d5199407301018e8b0
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-10 21:19:16 +0300

    17. Уроки React JS (остановочка "react за час, часть 1")

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -12,11 +12,9 @@ const MyPosts = () => {
                 <button>Remove</button>
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
+                <Post message="Hi, how are you?" likesCount="15"/>
+                <Post message="It's my first post" likesCount="20"/>
+                <Post message="It's my first post" likesCount="20"/>
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import s from './Post.module.css';
 
-const Post = () => {
+type PostPropsType = {
+    message: String;
+    likesCount: String;
+}
+
+const Post: React.FC<PostPropsType> = (props) => {
     return (
         <div className={s.item}>
             <img src="https://365psd.com/images/istock/previews/9730/97305669-avatar-icon-of-girl-in-a-baseball-cap.jpg"
                  alt="avatar_icon"/>
-            post1
+            {props.message}
+            <div>
+                <span>like</span>{props.likesCount}
+            </div>
         </div>
     );
 }
