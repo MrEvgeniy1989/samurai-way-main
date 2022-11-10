af33ff73959da02bcf1920dd4b53b829cd91b1c4
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-10 18:59:29 +0300

    update

---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from './Post.module.css';
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src="https://365psd.com/images/istock/previews/9730/97305669-avatar-icon-of-girl-in-a-baseball-cap.jpg"
+                 alt="avatar_icon"/>
+            post1
+        </div>
+    );
+}
+
+export default Post;
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import './Profile.css';
+import s from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img
                     src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
-                    alt="image1"/></div>
+                    alt="image1"/>
+            </div>
             <div>
                 ava + description
             </div>
-            <div>
-                My posts
-                <div>
-                    New post
-                </div>
-                <div>
-                    post1
-                </div>
-                <div>
-                    post2
-                </div>
-            </div>
+            <MyPosts/>
         </div>
 
     );
