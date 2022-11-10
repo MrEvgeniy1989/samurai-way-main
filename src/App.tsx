8eb8338c33440594b5081f4a8d6d2bc1568a84aa
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-10 16:27:31 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr"
+                    alt="Logo"/>
+            </header>
+            <nav className="nav">
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Settings</div>
+            </nav>
+            <div className="content">
+                <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="image1"/>
+            </div>
         </div>
     );
-}
+};
 
 export default App;
