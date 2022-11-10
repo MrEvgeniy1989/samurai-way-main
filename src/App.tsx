3f1e82456947c12de20976cd6fc49ea2c8f7dd1f
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-10 16:52:10 +0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
 
 const App = () => {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img
-                    src="https://play-lh.googleusercontent.com/DTzWtkxfnKwFO3ruybY1SKjJQnLYeuK3KmQmwV5OQ3dULr5iXxeEtzBLceultrKTIUTr"
-                    alt="Logo"/>
-            </header>
-            <nav className="nav">
-                <div>Profile</div>
-                <div>Messages</div>
-                <div>News</div>
-                <div>Music</div>
-                <div>Settings</div>
-            </nav>
+            <Header/>
+            <Navbar />
             <div className="content">
-                <img src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" alt="image1"/>
+                <div>
+                    <img
+                        src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+                        alt="image1"/></div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
             </div>
         </div>
     );
