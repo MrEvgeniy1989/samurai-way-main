a792eef57811a50d8672ab28e925e6653b8ae045
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-19 15:35:42 +0300

    22. Уроки React JS (Route exact) - react курсы бесплатно 19.11.2022

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import {NavLink} from 'react-router-dom';
 
 export const Dialogs = (props: any) => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + ' ' + s.active}>
-                    Evgenii
+                    <NavLink to='/dialogs/1'>Evgenii</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Dimych
+                    <NavLink to='/dialogs/2'>Dimych</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Andrey
+                    <NavLink to='/dialogs/3'>Andrey</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sveta
+                    <NavLink to='/dialogs/4'>Sveta</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sasha
+                    <NavLink to='/dialogs/5'>Sasha</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Victor
+                    <NavLink to='/dialogs/6'>Victor</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Valera
+                    <NavLink to='/dialogs/7'>Valera</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
