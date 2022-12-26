2a0a46411c44d46e2a2b1f8625aca57f79ab2135
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-12-26 21:43:17 +0300

    47 - React JS практика - Делаем копию состояния в ProfileReducer 26.12.2022 https://www.youtube.com/watch?v=L2SSsOmqiig&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=48&ab_channel=IT-KAMASUTRA

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,9 @@ import {App} from './App';
 import {store} from './redux/redux-store';
 import {Provider} from 'react-redux';
 
-const rerenderEntireTree = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>, document.getElementById('root'));
-}
-
-rerenderEntireTree()
-store.subscribe(() => {rerenderEntireTree()})
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>, document.getElementById('root'));
--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -23,18 +23,23 @@ const initialState: InitialStateType = {
 
 export const profileReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
     switch (action.type) {
-        case ADD_POST:
+        case ADD_POST: {
             const newPost: PostDataType = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0,
             }
-            state.posts.push(newPost)
-            state.newPostText = ''
-            return state
-        case UPDATE_NEW_POST_TEXT:
-            state.newPostText = action.newText
-            return state
+            let stateCopy = {...state}
+            stateCopy.posts = [...state.posts]
+            stateCopy.posts.push(newPost)
+            stateCopy.newPostText = ''
+            return stateCopy
+        }
+        case UPDATE_NEW_POST_TEXT: {
+            let stateCopy = {...state}
+            stateCopy.newPostText = action.newText
+            return stateCopy
+        }
         default:
             return state
     }
