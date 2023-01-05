4ba7af30beab83cec0cee0e14311c5413a03c8ed
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-01-05 20:11:49 +0300

    52 - React JS практика - Side Effect 05.01.2023 https://www.youtube.com/watch?v=O7o89_1sM5Y&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=53&ab_channel=IT-KAMASUTRA

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,16 +5,18 @@ import axios from 'axios';
 import userPhoto from '../../assets/images/user.png'
 
 export const Users = (props: UsersPropsType) => {
-
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            debugger
-            props.setUsers(response.data.items)
-        })
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                debugger
+                props.setUsers(response.data.items)
+            })
+        }
     }
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
@@ -34,8 +36,8 @@ export const Users = (props: UsersPropsType) => {
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{"u.location.country"}</div>
-                            <div>{"u.location.city"}</div>
+                            <div>{'u.location.country'}</div>
+                            <div>{'u.location.city'}</div>
                         </span>
                     </span>
                 </div>)
