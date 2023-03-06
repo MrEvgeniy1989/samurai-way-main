fae495e9ac5b2fcde062fb7f50a81fe03378d2b3
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-03-06 19:28:30 +0300

    63 - React JS - практика, DAL, axios.create
    06.03.2023
    
    https://www.youtube.com/watch?v=tZahQsOc9Jk&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=64
    
    https://youtu.be/tZahQsOc9Jk

---
+++ src/api/api.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': 'abf07995-8043-4f5a-aea7-fbec3de5488c'
+    }
+})
+
+export const usersAPI = {
+    getUsers(currentPage: number = 1, pageSize: number = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+    }
+}
