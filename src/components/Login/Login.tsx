7ee6ffd314885e1deecf88a7cfb04a39068e1e5d
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-03-21 00:37:38 +0300

    75 - React JS - redux-form введение (login)
    21.03.2023
    
    https://www.youtube.com/watch?v=yhBYEM3VDTk&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=76
    
    https://youtu.be/yhBYEM3VDTk
    
    Типизация:
    https://youtu.be/4kxvSDRQOko

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,11 +1,54 @@
 import React, {FC} from 'react';
+import style from './Login.module.css'
+import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 
-type LoginPropsType = {
+type LoginFormPropsType = {}
 
+type FormDataType = {
+  login: string
+  password: string
+  rememberMe: boolean
 }
+
+export const LoginForm: FC<InjectedFormProps<FormDataType> & LoginFormPropsType> = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <div>
+        <Field placeholder={'Login'} name={'login'} component={'input'}/>
+      </div>
+
+      <div>
+        <Field placeholder={'Password'} name={'password'} component={'input'}/>
+      </div>
+
+      <div>
+        <Field component={'input'} name={'rememberMe'} type="checkbox"/> Remember me
+      </div>
+
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  )
+}
+
+
+const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
+
+
+type LoginPropsType = {}
 
 export const Login: FC<LoginPropsType> = (props) => {
-    return (
-        <h1>LOGIN</h1>
-    )
+
+  const onSubmit = (formData: FormDataType) => {
+
+  }
+
+  return (
+    <div className={style.login}>
+      <h1>Login</h1>
+      <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
+  )
 }
+
