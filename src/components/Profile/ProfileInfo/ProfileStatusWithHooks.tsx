da12a15c591b6026114ca49b47aa7ca617eec3a6
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-03-22 21:32:44 +0300

    84 - React JS - hook, useState, хуки
    22.03.2023
    
    https://www.youtube.com/watch?v=oWeSh6-Mrvg&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=85
    
    https://youtu.be/EbqQg0K4wbo
    
    Типизация:

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,41 @@
+import React, {ChangeEvent, FC, useEffect, useState} from "react";
+
+type ProfileStatusWithHooksPropsType = {
+  status: string
+  updateStatus: (status: string) => void
+}
+export const ProfileStatusWithHooks: FC<ProfileStatusWithHooksPropsType> = ({status, updateStatus}) => {
+
+  const [editMode, setEditMode] = useState<boolean>(false)
+  const [newStatus, setNewStatus] = useState<string>(status)
+
+  useEffect(() => {
+    if (status !== newStatus)
+      setNewStatus(status)
+  }, [status])
+
+  const deActivateEditMode = () => {
+    setEditMode(false)
+    updateStatus(newStatus)
+  }
+  const activateEditMode = () => {
+    setEditMode(true)
+  }
+  const onStatusChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setNewStatus(e.currentTarget.value)
+  }
+  return (
+    <div>
+      Status:
+      {
+        !editMode
+          ? <div>
+            <span onDoubleClick={activateEditMode}>{status || '---'}</span>
+          </div>
+          : <div>
+            <input onChange={onStatusChange} value={newStatus} onBlur={deActivateEditMode} autoFocus/>
+          </div>
+      }
+    </div>
+  )
+}
