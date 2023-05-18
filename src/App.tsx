6f49140253a761a5f16337fe2202795ad4d835a9
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-05-18 23:36:26 +0300

    92 - тестируем компоненты, тесты, react-test-renderer - React JS
    18.05.2023
    
    https://www.youtube.com/watch?v=Kyc_Z_2b2Hc&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=93
    
    https://youtu.be/Kyc_Z_2b2Hc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, {ComponentType} from 'react';
 import './App.css';
 import {Navbar} from './components/Navbar/Navbar';
-import {Route, withRouter} from 'react-router-dom';
+import {BrowserRouter, Route, withRouter} from 'react-router-dom';
 import {News} from './components/News/News';
 import {Music} from './components/Music/Music';
 import {Settings} from './components/Settings/Settings';
@@ -10,55 +10,67 @@ import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import Login from './components/Login/Login';
-import {connect} from 'react-redux';
-import {AppStateType} from './redux/redux-store';
+import {connect, Provider} from 'react-redux';
+import {AppStateType, store} from './redux/redux-store';
 import {compose} from 'redux';
 import {initializeApp} from './redux/app-reducer';
 import {Preloader} from './components/common/Preloader/Preloader';
 
 type MapStateToPropsType = {
-  initialized: boolean
+    initialized: boolean
 }
 type MapDispatchToPropsType = {
-  initializeApp: () => void
+    initializeApp: () => void
 }
 type AppPropsType = MapStateToPropsType & MapDispatchToPropsType
 
 
 class App extends React.Component<AppPropsType, AppStateType> {
 
-  componentDidMount() {
-    this.props.initializeApp()
-  }
-
-  render() {
-    if (!this.props.initialized) {
-      return <Preloader/>
+    componentDidMount() {
+        this.props.initializeApp()
     }
-    return (
-      <div className="app-wrapper">
-        <HeaderContainer/>
-        <div className={'appContainer'}>
-          <Navbar/>
-          <div className="app-wrapper-content">
-            <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
-            <Route path="/dialogs" render={() => <DialogsContainer/>}/>
-            <Route path="/users" render={() => <UsersContainer/>}/>
-            <Route path="/news" render={() => <News/>}/>
-            <Route path="/music" render={() => <Music/>}/>
-            <Route path="/settings" render={() => <Settings/>}/>
-            <Route path="/login" render={() => <Login/>}/>
-          </div>
-        </div>
-      </div>
-    )
-  }
+
+    render() {
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
+        return (
+            <div className="app-wrapper">
+                <HeaderContainer/>
+                <div className={'appContainer'}>
+                    <Navbar/>
+                    <div className="app-wrapper-content">
+                        <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
+                        <Route path="/dialogs" render={() => <DialogsContainer/>}/>
+                        <Route path="/users" render={() => <UsersContainer/>}/>
+                        <Route path="/news" render={() => <News/>}/>
+                        <Route path="/music" render={() => <Music/>}/>
+                        <Route path="/settings" render={() => <Settings/>}/>
+                        <Route path="/login" render={() => <Login/>}/>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state: AppStateType) => ({
-  initialized: state.app.initialized
+    initialized: state.app.initialized
 })
 
-export default compose<ComponentType>(
-  withRouter,
-  connect(mapStateToProps, {initializeApp}))(App)
+let AppContainer = compose<ComponentType>(
+    withRouter,
+    connect(mapStateToProps, {initializeApp}))(App)
+
+const SamuraiJSApp = () => {
+    return (
+        <BrowserRouter>
+            <Provider store={store}>
+                <AppContainer/>
+            </Provider>
+        </BrowserRouter>
+    )
+}
+
+export default SamuraiJSApp;
