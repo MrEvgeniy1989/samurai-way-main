da705e66b6045fa4681da5a0021edc6e059b955f
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-05-19 18:20:52 +0300

    Updates

---
+++ static/js/610.028b04ea.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksamurai_way=self.webpackChunksamurai_way||[]).push([[610],{610:function(n,u,e){e.r(u),e.d(u,{News:function(){return s}});e(2791);var r=e(184),s=function(n){return(0,r.jsx)("div",{children:"News"})}}}]);
+//# sourceMappingURL=610.028b04ea.chunk.js.map
